dcd001272f5cc08fdfa741606662fd2a82b4fc06
largezhou <largezhou@hotmail.com>
2021-12-22 22:57:32 +0800

    配置全局 Vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import commonjs from '@rollup/plugin-commonjs'
+import externalGlobals from 'rollup-plugin-external-globals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +10,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['vue'],
-      output: {
-        globals: {
+      plugins: [
+        commonjs(),
+        externalGlobals({
           vue: 'Vue',
-        },
-      },
+        }),
+      ],
     },
   },
   resolve: {
